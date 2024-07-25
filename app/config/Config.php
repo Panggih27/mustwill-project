f982700b6fbca0e6e17a1a3a75fcc1430540526a
Panggih27 <projectdigital32@gmail.com>
2024-07-25 20:31:56 +0700

    merubah konfigurasi pd Config

--- app/config/Config.php
+++ app/config/Config.php
@@ -1,10 +1,10 @@
 <?php
 
-define('BASEURL', 'http://localhost/mustwill/public');
+define('BASEURL', 'http://pasangacmurah.com/public');
 require_once '../app/config/Config.php';
 
 // DB
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'mustwill');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'localhost'); // Biasanya 'localhost', tapi bisa bervariasi tergantung hosting
+define('DB_NAME', 'pasp5526_dbmustwill');
+define('DB_USER', 'pasp5526_user-dbmustwill');
+define('DB_PASS', 'L9&XX3F~o7xz');
